e661ea7215aa7f51fb5f8522abb82bb1b918b26c
David Marvasti <david.marvasti@wework.com>
2018-07-19 11:52:58 -0400

    Add finished badge to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@
 import * as React from 'react';
 import { AuthShape } from "../redux/auth/authShape";
 import { Avatar } from 'antd';
-import { Button, Layout, Row, Col } from 'antd';
+import { Button, Layout, Row, Col, Tag } from 'antd';
 import Media from "react-media";
+import styled from "styled-components";
 
 // Redux provided props via mapStateToProps
 interface HeaderProps {
   authd: AuthShape
   giphy: any;
   isFinished: boolean;
+  className?: string;
   onLogin: () => void;
   onLogout: () => void;
   onExecuteLottery: () => void;  
@@ -18,20 +20,24 @@ interface HeaderProps {
 
 const { Header } = Layout;
 
-class LotteryHeader extends React.Component<HeaderProps> {
+class LotteryHeaderBase extends React.Component<HeaderProps> {
   public render() {
     return (
-      <div>
+      <div className={this.props.className}>
+        {
+          this.props.authd.isAuthd && (
+            <Avatar size="large" src={this.props.authd.user.picture} />
+          )
+        }
 
         <Row type="flex" justify="center" className={"splash"}>
           <Col xs={24} md={this.props.authd.isAuthd ? 14 : 24}>
-            {
-              this.props.authd.isAuthd && (
-                <Avatar size="large" src={this.props.authd.user.picture} />
-              )
-            }
-
-            <h1>Massage Lottery</h1>
+            <h1>
+              Massage Lottery 
+              {
+                this.props.isFinished && <Tag color="red">Lottery Closed</Tag>
+              }
+            </h1>
            
             {
               !this.props.authd.isAuthd ? (
@@ -89,4 +95,28 @@ class LotteryHeader extends React.Component<HeaderProps> {
   }
 }
 
-export default LotteryHeader;
+export default styled(LotteryHeaderBase)`
+  .ant-avatar {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+
+    @media (max-width: 500px) {
+      height: 25px;
+      width: 25px;
+      top: 15px;
+    }
+  }
+
+  h1 {
+    .ant-tag {
+      margin-left: 5px;
+    }
+  }
+
+  @media (max-width: 500px) {
+    h1 {
+      font-size: 1.5em;
+    }
+  }
+`
